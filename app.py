de5e4ac1fc8a33542917f351499c01cd90cd363e
root <root@ip-172-31-3-111.ap-south-1.compute.internal>
2025-09-09 10:18:00 +0000

    initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, DevOps This is CI/CD World!"
+    return "Flask CI/CD Web App is running successfully on AWS!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
